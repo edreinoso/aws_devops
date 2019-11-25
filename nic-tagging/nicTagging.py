869b732eb503ab2eb2791732444cc52c6ef5bfaa
edreinoso <eqr5172@psu.edu>
2019-11-25 09:46:08 -0500

    changed the nic tagging module to fix some bugs

--- nic-tagging/nicTagging.py
+++ nic-tagging/nicTagging.py
@@ -11,81 +11,125 @@ eni_ids = []
 
 def lambda_handler(event, context):
     # function variables
+    d = 0
     x = 0  # this is to take more than one instance
     y = 0  # this would be to go inside of the eni variable
+    len_tags = 0
+    value = ''
     tag_class = createTag()
     areThereEC2Tags = 'Tags'
-
+    
     client = boto3.client('ec2', event['region'])
 
-    ec2 = client.describe_instances()
+    ec2 = client.describe_instances(
+        Filters=[
+            # {
+            #     'Name': 'vpc-id',
+            #     'Values': [event['']]
+            # },
+            # {
+            #     'Name': 'tag:Test',
+            #     'Values': ['Y']
+            # },
+        ],
+        
+        # Testing instances
+        # InstanceIds=['i-03a11bb0c6c685a89'],
+        # InstanceIds=['i-0f8ec86c0942f89f3','i-03a11bb0c6c685a89'],
+        # InstanceIds=['i-0f8ec86c0942f89f3'],
+        # InstanceIds=['i-08a90ef3eebcc15dc'],
+        # InstanceIds=['i-071f3073e22a4dbeb'],
+    )
+    
+    print('\n')
 
     for reservation in ec2["Reservations"]:
-
+        
         # print(reservation)
 
         for instance in reservation["Instances"]:
 
-            # print('instance id: ' + instance['InstanceId'])
+            print('instance id: ' + instance['InstanceId'])
 
             # 1st part: Put the instance tags in a variable in order
             # to do some comparison
             if areThereEC2Tags in instance:
-                print(
-                    'There is a tag inside of this instance')
+                # print('There is a tag inside of this instance')
+                len_tags = len(instance['Tags'])
                 for tags in instance["Tags"]:
+                    # print(type(tags))
+                    # print('length of tags: ' + str(len_tags))    
+                    # print('this is D: ' + str(d))
+                    print('tag key: ' + tags['Key'] + ' tag value: ' + tags['Value'])
                     if (tags['Key'] == 'Name'):
+                        # value = tags['Value']
+                        
                         ec2_tag_key.append(tags['Key'])
                         ec2_tag_value.append(tags['Value'])
-                        print('ec2 tag - key: ' +
-                              ec2_tag_key[x] + ' value: ' + ec2_tag_value[x])
+                        # print(type(ec2_tag_value))
+                        # print(len(ec2_tag_value))
+                        print('\tYY - ec2 tag - key: ' + ec2_tag_key[x] + ' value: ' + ec2_tag_value[x])
+                        break
+                    elif (d >= len_tags-1):
+                    # elif (tags['Key'] != 'Name'):
+                    # # elif (d >= len_tags-1 and tags['Key'] != 'Name'):
+                        print('d is last and no name tag for ec2 instance')
+                        ec2_tag_key.append('Name')
+                        ec2_tag_value.append('')
+                        print('\tXX - ec2 tag - key: ' + ec2_tag_key[x] + ' value: ' + ec2_tag_value[x])
+                    print('\n')
+                    d += 1
+                d = 0
+
 
                 # 2nd part: Get the ENI of those ec2 instances
                 # and store in variable
                 for nic in instance["NetworkInterfaces"]:
                     eni_ids.append(nic['NetworkInterfaceId'])
                     print('eni id: ' + eni_ids[y])
-
+                    
                     ec2_eni = client.describe_network_interfaces(
                         NetworkInterfaceIds=[
                             eni_ids[y]
                         ],
                     )
+                    
                     for networkinterfaces in ec2_eni['NetworkInterfaces']:
                         # testing line
-                        # print('Size of tagset ' +
-                        #       str(len(networkinterfaces['TagSet'])))
-                        # print('network interface id: ' +
-                        #       networkinterfaces['NetworkInterfaceId'])
-
+                        # print('Size of tagset ' + str(len(networkinterfaces['TagSet'])))
+                        print('network interface id: ' + networkinterfaces['NetworkInterfaceId'])
+                    
+                        # this if would help as an error checker.
                         if (len(networkinterfaces['TagSet']) > 0):
                             for nic_tags in networkinterfaces['TagSet']:
                                 # We want to check for the key 'name'
                                 if (nic_tags['Key'] == 'Name'):
                                     # if the value is not equal the ec2 instance tag
-                                    if(nic_tags['Value'] != ec2_tag_value[x]):
-                                        # testing lines
-                                        # print(
-                                        #     'ENI name tag is not same as the ec2 instance name tag')
-                                        # assign a value for that specific key
-                                        # based on those resources obtained in the step 2
-
-                                        tag_class.create_tag(
-                                            client, eni_ids[y], ec2_tag_key[x], ec2_tag_value[x])
-                                    # testing lines
-                                    # print('This loop gave me the Name tag')
-                                print('nic tag - key: ' +
-                                      nic_tags['Key'] + ' value: ' + nic_tags['Value'])
+                                    # print('len(ec2_tag_value): '+ str(len(ec2_tag_value)))
+                                    # if(len(ec2_tag_value) > 0):
+                                    if (ec2_tag_value[x] != ""):
+                                        print('Not empty!!!')
+                                        print('\tnic card name: ' + nic_tags['Value'] + ' - ec2-tag: ' + ec2_tag_value[x])
+                                        if(nic_tags['Value'] != ec2_tag_value[x]):
+                                            # testing lines
+                                            print('####ENI name tag is not same as the ec2 instance name tag####')
+                                            tag_class.create_tag(client, eni_ids[y], ec2_tag_key[x], ec2_tag_value[x])
+                                    else:
+                                        print('Empty!!!')
+                                        print('\tnic card name: ' + nic_tags['Value'])
+                                        tag_class.create_tag(client, eni_ids[y], 'Name', '')
                         else:
-                            tag_class.create_tag(
-                                client, eni_ids[y], ec2_tag_key[x], ec2_tag_value[x])
-                    y += 1
+                            print('there are no tags in this nic, creating one ')
+                            if (ec2_tag_value[x] != ""):
+                                tag_class.create_tag(client, eni_ids[y], ec2_tag_key[x], ec2_tag_value[x])
+                            else:
+                                tag_class.create_tag(client, eni_ids[y], 'Name', '')
+                    y += 1 # to loop to all the nics in an ec2 instance
 
             else:
-                print('Please assign a tag to the instance: ' +
-                      instance['InstanceId'])
+                print('Please assign a tag to the instance: ' + instance['InstanceId'])
 
-            x += 1
+            x += 1 # to loop through all the instances
             print(x)
             print('\n')
 
--- security-group-checker/sg.py
+++ security-group-checker/sg.py
@@ -6,6 +6,7 @@ range_170 = '170.0.0.0/8'
 range_172 = '172.0.0.0/8'
 range_167 = '167.0.0.0/8'  # this range is going to have to be changed to 167.219.0.0/16
 range_140 = '140.0.0.0/8'
+range_10 = '10.0.0.0/8'
 
 # exception handling variables
 exFromPort = 'FromPort'
@@ -16,7 +17,7 @@ client = boto3.client('ec2', region_name=region)
 
 # data will be sent to S3 for analysis
 bucket_name = "security-groups-checker"
-file_name = "tstSecurityGroups.txt"
+file_name = "mgtSecurityGroups.txt"
 lambda_path = "/tmp/" + file_name
 s3_path = "/clean_up_nov_19/" + file_name
 
@@ -28,7 +29,7 @@ def lambda_handler(event, context):
             {
                 'Name': 'vpc-id',
                 # this should just be a variable that's passed with the event
-                'Values': [event['tst']]
+                'Values': [event['mgt']]
             },
         ]
     )
@@ -101,6 +102,17 @@ def lambda_handler(event, context):
                                    toPort, ipProtocol, range_167)
                         applyRule(response, groupId, fromPort, toPort,
                                   ipProtocol, '167.219.0.0/16', 'Deloitte Network')
+                    
+                    # If the program goes to the 10/8 range, change it for 10.139/16
+                    if cidr['CidrIp'] == range_10:
+                        # print('\t\tHello World: 10 security group')
+                        string = writeToFile(
+                            num, groupId, groupName, fromPort, toPort, ipProtocol, range_10)
+                        finalString += string
+                        deleteRule(response, groupId, fromPort,
+                                   toPort, ipProtocol, range_10)
+                        applyRule(response, groupId, fromPort, toPort,
+                                  ipProtocol, '10.139.0.0/16', 'AWS Network')
     #     print('\n')
     # print('\n')
 
