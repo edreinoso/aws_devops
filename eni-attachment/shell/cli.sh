a4fa89a9e99b16226d63e1440cb8709a8d6cbb75
Edgardo Reinoso <e.reinoso@azerion.com>
2021-10-26 23:20:45 +0200

    experimentations with the ssm.json config file, launching a new centos instance and attaching secondary eni

--- eni-attachment/shell/cli.sh
+++ eni-attachment/shell/cli.sh
@@ -1,7 +1,7 @@
 # cli for detaching and attaching network interface
 ## create, attach, detach
 aws ec2 create-network-interface --cli-input-json
-aws ec2 attach-network-interface --device-index 2 --instance-id i-0100d47078f733d77 --network-interface-id eni-0bb80bee08461d130
+aws ec2 attach-network-interface --device-index 2 --instance-id i-0100d47078f733d77 --network-interface-id eni-0135db642d8a403f2
 aws ec2 detach-network-interface --attachment-id eni-attach-04c478f8e6bc17d91
 
 
@@ -18,18 +18,9 @@ aws ssm create-document \
     --document-type "Command" \
     --tags "Key=Name,Value=eni-attachment; Key=Template,Value=aws_devops; Key=Environment,Value=dev; Key=Application,Value=eni-attachment; Key=Purpose,Value=aws_automation_devops; Key=Creation_Date,Value=Aug_06_2021"
 
-aws ssm update-document \
-    --name "eni-attachment" \
-    --content file:///Users/elchoco/aws/functions/eni-attachment/shell/eni_attachment_ssm.json \
-    --document-version '$LATEST'
-
-aws ssm update-document-default-version \
-    --name "eni-attachment" \
-    --document-version "2"
-
 ==
 
-aws ssm update-document --name "eni-attachment" --content file://config_ssm.json --document-version '$LATEST'; aws ssm update-document-default-version --name "eni-attachment" --document-version "16"
+aws ssm update-document --name "eni-attachment" --content file://ssm.json --document-version '$LATEST'; aws ssm update-document-default-version --name "eni-attachment" --document-version "17"
 
 
 aws ssm send-command --instance-ids [] --document-name "eni-attachment" --document-version "10" --parameters "commands=['']"
--- eni-attachment/shell/script.sh
+++ eni-attachment/shell/script.sh
@@ -18,7 +18,7 @@ EOF
 # this commands restarts teh network config in instance
 systemctl restart network.service
 
-sleep 10
+sleep 10 # there is a possibility here where restarting the network service might take longer. However, currently this is static and might need some improvements
 
 eni0=`hostname -I | awk '{print $1}'`
 eni1=`hostname -I | awk '{print $2}'`
@@ -26,13 +26,11 @@ eni1=`hostname -I | awk '{print $2}'`
 # perform the necessary routing changes
 ip rule add from $eni0/32 lookup 100
 ip rule add from $eni1/32 lookup 200
-
 ip route add default via $gw dev eth0 table 100
 ip route add default via $gw dev eth1 table 200
 
 # persisting changes
 echo "from $eni0/32 lookup 100" >> /etc/sysconfig/network-scripts/rule-eth0
 echo "default via $gw dev eth0 table 100" >> /etc/sysconfig/network-scripts/route-eth0
-
 echo "from $eni1/32 lookup 200" >> /etc/sysconfig/network-scripts/rule-eth1
 echo "default via $gw dev eth1 table 200" >> /etc/sysconfig/network-scripts/route-eth1
