5b26ae3a400861f904bc14ba142ff9cb24ce68bc
Ed Reinoso <edgardojesus16@gmail.com>
2022-07-14 14:43:15 +0200

    appropriate class naming

--- compute/start-stop/pi-stop-start-rds/lambda_function.py
+++ compute/start-stop/pi-stop-start-rds/lambda_function.py
@@ -1,33 +1,34 @@
 import boto3
-from stop import stop
-from start import start
+from stop import stopRDS
+from start import startRDS
 
 
 def lambda_handler(event, context):
-    print ('\n')
+    print('\n')
     client = boto3.client('rds')
-    stopInstance = stop()
-    startInstance = start()
+    stopInstance = stopRDS()
+    startInstance = startRDS()
     status = ''
-    
+
     for instance in event['instances']:
-        rdsInstance = client.describe_db_instances(DBInstanceIdentifier=instance)
+        rdsInstance = client.describe_db_instances(
+            DBInstanceIdentifier=instance)
         for description in rdsInstance['DBInstances']:
             status = description['DBInstanceStatus']
-        print (instance)
+        print(instance)
         # depending on the event
         # the system is supposed to stop (at night 11pm)
         # or start the RDS instance (8am)
         if (event['action'] == 'start'):
             print('starting instance:' + instance)
-            if (status == 'available'): 
+            if (status == 'available'):
                 print('rds status is available already')
             else:
                 print('starting instance:' + instance)
                 startInstance.start_rds(instance, client)
         elif (event['action'] == 'stop'):
             print('RDS instance status: ' + status)
-            if (status == 'stopped'): 
+            if (status == 'stopped'):
                 print('rds status is stopped already')
             else:
                 print('stopping instance:' + instance)
@@ -37,4 +38,4 @@ def lambda_handler(event, context):
             # implement a swich statement: it could actually
             # easier to do an if statements, but for the sake
             # of learning, let's implement a switch
-        print ('\n')
+        print('\n')
